f17fe2e0812f1adca0f8a9d111bec50e6cfc53a7
Mohammad Hussein <mohammad.hussein377@gmail.com>
2023-08-09 13:25:39 +0300

    created the feed component

--- Client/src/components/Feed/index.jsx
+++ Client/src/components/Feed/index.jsx
@@ -1,4 +1,9 @@
+import {BiDotsHorizontalRounded} from "react-icons/bi";
 import "./style.css"
+import {AiOutlineHeart} from "react-icons/ai";
+import {FaRegComment} from "react-icons/fa"
+import {IoPaperPlaneOutline} from "react-icons/io5"
+import {FiBookmark} from "react-icons/fi"
 
 const index = () => {
     return (
@@ -28,6 +33,122 @@ const index = () => {
                                 alt=""/>
                         </div>
                         <div className="name">mhmd_hussein</div>
+                    </div>
+                </div>
+                <div className="posts">
+                    <div className="post">
+                        <div className="profile">
+                            <div className="left">
+                                <div className="profile-pic">
+                                    <img
+                                        src="https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?w=900&t=st=1691570136~exp=1691570736~hmac=576643e1b7fe42a13cba560394e3fec7ba404f7c5054f9cb6adbc5beaf86357f"
+                                        alt=""/>
+                                </div>
+                                <div className="username">mhmd_hussein</div>
+                            </div>
+                            <div className="dots">
+                                <BiDotsHorizontalRounded size={30}/>
+                            </div>
+                        </div>
+                        <div className="post-content">
+                            <div className="post-pic">
+                                <img
+                                    src="https://img.freepik.com/premium-photo/forest-river-with-reflection-trees-water-digital-painting_890887-7465.jpg?w=740"
+                                    alt=""/>
+                            </div>
+                            <div className="reactions">
+                                <div className="left">
+                                    <div className="icon heart">
+                                        <AiOutlineHeart size={25}/>
+                                    </div>
+                                    <div className="icon comment">
+                                        <FaRegComment size={25}/>
+                                    </div>
+                                    <div className="icon share">
+                                        <IoPaperPlaneOutline size={25}/>
+                                    </div>
+                                </div>
+                                <div className="right">
+                                    <FiBookmark className="icon" size={25}/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="post">
+                        <div className="profile">
+                            <div className="left">
+                                <div className="profile-pic">
+                                    <img
+                                        src="https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?w=900&t=st=1691570136~exp=1691570736~hmac=576643e1b7fe42a13cba560394e3fec7ba404f7c5054f9cb6adbc5beaf86357f"
+                                        alt=""/>
+                                </div>
+                                <div className="username">mhmd_hussein</div>
+                            </div>
+                            <div className="dots">
+                                <BiDotsHorizontalRounded size={30}/>
+                            </div>
+                        </div>
+                        <div className="post-content">
+                            <div className="post-pic">
+                                <img
+                                    src="https://img.freepik.com/premium-photo/forest-river-with-reflection-trees-water-digital-painting_890887-7465.jpg?w=740"
+                                    alt=""/>
+                            </div>
+                            <div className="reactions">
+                                <div className="left">
+                                    <div className="icon heart">
+                                        <AiOutlineHeart size={25}/>
+                                    </div>
+                                    <div className="icon comment">
+                                        <FaRegComment size={25}/>
+                                    </div>
+                                    <div className="icon share">
+                                        <IoPaperPlaneOutline size={25}/>
+                                    </div>
+                                </div>
+                                <div className="right">
+                                    <FiBookmark className="icon" size={25}/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="post">
+                        <div className="profile">
+                            <div className="left">
+                                <div className="profile-pic">
+                                    <img
+                                        src="https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?w=900&t=st=1691570136~exp=1691570736~hmac=576643e1b7fe42a13cba560394e3fec7ba404f7c5054f9cb6adbc5beaf86357f"
+                                        alt=""/>
+                                </div>
+                                <div className="username">mhmd_hussein</div>
+                            </div>
+                            <div className="dots">
+                                <BiDotsHorizontalRounded size={30}/>
+                            </div>
+                        </div>
+                        <div className="post-content">
+                            <div className="post-pic">
+                                <img
+                                    src="https://img.freepik.com/premium-photo/forest-river-with-reflection-trees-water-digital-painting_890887-7465.jpg?w=740"
+                                    alt=""/>
+                            </div>
+                            <div className="reactions">
+                                <div className="left">
+                                    <div className="icon heart">
+                                        <AiOutlineHeart size={25}/>
+                                    </div>
+                                    <div className="icon comment">
+                                        <FaRegComment size={25}/>
+                                    </div>
+                                    <div className="icon share">
+                                        <IoPaperPlaneOutline size={25}/>
+                                    </div>
+                                </div>
+                                <div className="right">
+                                    <FiBookmark className="icon" size={25}/>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
