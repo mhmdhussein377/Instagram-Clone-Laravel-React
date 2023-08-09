52524097492f4a9648ac7d92ae122ecffe5e2d0c
Mohammad Hussein <mohammad.hussein377@gmail.com>
2023-08-09 11:56:06 +0300

    created the structure of the friends component

--- Client/src/components/Friends/index.jsx
+++ Client/src/components/Friends/index.jsx
@@ -2,8 +2,66 @@ import "./style.css"
 
 const index = () => {
     return (
-        <div className="friends">Friends</div>
-    )
+        <div className="friends">
+            <div className="my-profile">
+                <div className="profile-pic">
+                    <img
+                        src="https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?w=900&t=st=1691570136~exp=1691570736~hmac=576643e1b7fe42a13cba560394e3fec7ba404f7c5054f9cb6adbc5beaf86357f"
+                        alt=""/>
+                </div>
+                <div className="data">
+                    <div className="username">mhmd_hussein</div>
+                    <div className="name">Mohammad Hussein</div>
+                </div>
+            </div>
+            <div className="friends-list">
+                <div className="profile">
+                    <div className="profile-pic">
+                        <img
+                            src="https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?w=900&t=st=1691570136~exp=1691570736~hmac=576643e1b7fe42a13cba560394e3fec7ba404f7c5054f9cb6adbc5beaf86357f"
+                            alt=""/>
+                    </div>
+                    <div className="data">
+                        <div className="username">mhmd_hussein</div>
+                        <div className="name">Mohammad Hussein</div>
+                    </div>
+                </div>
+                <div className="profile">
+                    <div className="profile-pic">
+                        <img
+                            src="https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?w=900&t=st=1691570136~exp=1691570736~hmac=576643e1b7fe42a13cba560394e3fec7ba404f7c5054f9cb6adbc5beaf86357f"
+                            alt=""/>
+                    </div>
+                    <div className="data">
+                        <div className="username">mhmd_hussein</div>
+                        <div className="name">Mohammad Hussein</div>
+                    </div>
+                </div>
+                <div className="profile">
+                    <div className="profile-pic">
+                        <img
+                            src="https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?w=900&t=st=1691570136~exp=1691570736~hmac=576643e1b7fe42a13cba560394e3fec7ba404f7c5054f9cb6adbc5beaf86357f"
+                            alt=""/>
+                    </div>
+                    <div className="data">
+                        <div className="username">mhmd_hussein</div>
+                        <div className="name">Mohammad Hussein</div>
+                    </div>
+                </div>
+                <div className="profile">
+                    <div className="profile-pic">
+                        <img
+                            src="https://img.freepik.com/free-photo/portrait-white-man-isolated_53876-40306.jpg?w=900&t=st=1691570136~exp=1691570736~hmac=576643e1b7fe42a13cba560394e3fec7ba404f7c5054f9cb6adbc5beaf86357f"
+                            alt=""/>
+                    </div>
+                    <div className="data">
+                        <div className="username">mhmd_hussein</div>
+                        <div className="name">Mohammad Hussein</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default index
